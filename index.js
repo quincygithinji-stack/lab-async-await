f38b7e0538223a462a43b0ba6eee04ad9348a340
Quincy <quincy.githinji@student.moringaschool.com>
2026-07-22 20:08:57 +0300

    Complete random posts async await lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here!
+async function getPosts() {
+    try {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        const posts = await response.json();
+
+        const postList = document.querySelector("#post-list");
+
+        posts.slice(0, 10).forEach((post) => {
+            const li = document.createElement("li");
+
+            const title = document.createElement("h1");
+            title.textContent = post.title;
+
+            const body = document.createElement("p");
+            body.textContent = post.body;
+
+            li.appendChild(title);
+            li.appendChild(body);
+
+            postList.appendChild(li);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getPosts();
